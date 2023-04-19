83a6514786fab81d6015142cd2b53ce7030d335b
Sergio E <92735991+Serg-ioE@users.noreply.github.com>
2023-04-19 13:50:18 -0400

    4/19/2023 End of class
    fixed fontawesome icon

--- main.js
+++ main.js
@@ -1,14 +1,6 @@
-var navButtonOpen = false;
-
 $(document).ready(function () {
-  navButtonOpen = false;
   $(".navexpanded").hide(); // hide the navexpanded div on page load
   $(".navbar-toggler").click(function () {
     $(".navexpanded").slideToggle();
   });
 });
-  /*navButtonOpen = !navButtonOpen;
-  if ((navButtonOpen = true)) {
-    
-  } else {
-  }*/

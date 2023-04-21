efa22e1461391421fa3993efa7ba9c867b245052
Sergio E <92735991+Serg-ioE@users.noreply.github.com>
2023-04-21 11:56:06 -0400

    end of friday

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
+//var mainNavHidden =
 $(document).ready(function () {
   $(".navexpanded").hide(); // hide the navexpanded div on page load
   $(".navbar-toggler").click(function () {
     $(".navexpanded").slideToggle();
+    $(".navbar").slideToggle();
   });
 });
+// Detect when the window is resized
+window.addEventListener('resize', function() {
+  // Get the current viewport width and height
+  var viewportWidth = window.innerWidth
+  // Check if the viewport width and height reach sm size (bootstrap: 576px)
+  if (viewportWidth >= 576 ) {
+    $(".navexpanded").slideToggle();
+    $(".navbar").slideToggle();
+    console.log('Viewport was bigger than sm while navexpanded was open');
+    return
+  }
+});
+

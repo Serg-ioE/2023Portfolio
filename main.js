fa9dc8c2f69419493cfda95fa924d8138631551f
Sergio E <92735991+Serg-ioE@users.noreply.github.com>
2023-04-24 21:33:08 -0400

    Monday, April 24 from home
    Forgot what I got done today but it was a good amount of stuff. *Thumbs up*

--- main.js
+++ main.js
@@ -1,21 +1,49 @@
-//var mainNavHidden =
 $(document).ready(function () {
+  
   $(".navexpanded").hide(); // hide the navexpanded div on page load
   $(".navbar-toggler").click(function () {
     $(".navexpanded").slideToggle();
     $(".navbar").slideToggle();
   });
+  
+
+
+  var prevPage;
+  
+  $(".navbar-button").click(function () {
+    var page = Math.floor(Math.random() * 6)
+    while (prevPage == page) {
+      page = Math.floor(Math.random() * 6);
+      console.log("it was the same this amount of times");
+    }
+    switch (page) {
+      case 0:
+        window.location.href = "index.html";
+        console.log("index");
+       break;
+      case 1:
+        window.location.href = "about.html";
+        console.log("about");
+       break;
+      case 2:
+        window.location.href = "education.html";
+        console.log("education");
+       break;
+      case 3:
+        window.location.href = "work.html";
+        console.log("work");
+       break;
+      case 4:
+        window.location.href = "interests.html";
+        console.log("interests");
+       break;
+      case 5:
+        window.location.href = "contact.html";
+        console.log("contact");
+       break;
+    }
+    prevPage = page;
+  });
 });
-// Detect when the window is resized
-window.addEventListener('resize', function() {
-  // Get the current viewport width and height
-  var viewportWidth = window.innerWidth
-  // Check if the viewport width and height reach sm size (bootstrap: 576px)
-  if (viewportWidth >= 576 ) {
-    $(".navexpanded").slideToggle();
-    $(".navbar").slideToggle();
-    console.log('Viewport was bigger than sm while navexpanded was open');
-    return
-  }
-});
+
 

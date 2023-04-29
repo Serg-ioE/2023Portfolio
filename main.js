5becc5b9c267b5b2017cb5c0ce534ffaf51d5613
Sergio E <92735991+Serg-ioE@users.noreply.github.com>
2023-04-28 20:35:20 -0400

    Fully Completed Navbar (as of right now at least)
    10+ hours on a custom navbar..... at least I taught myself so many new things :) (Yes future Sergio the navbar.height + hero.height = viewport height at all times now, only in index though) (uses javascript, jquery, css, and css flex) So Happy right now

--- main.js
+++ main.js
@@ -1,17 +1,41 @@
 $(document).ready(function () {
-  
-  $(".navexpanded").hide(); // hide the navexpanded div on page load
+  // V V Controls the navbar hiding and showing with the correct Css
+  $(".navexpanded").hide();
+
+  var decider = false; // false = navexpanded is closed, true = navexpanded is open
+
   $(".navbar-toggler").click(function () {
     $(".navexpanded").slideToggle();
+    decider = !decider;
+    $(".navexpanded").css("display", "flex");
+    if (decider == true) {
+      $("#navbar-toggler-navexpanded").css("margin-left", "20%");
+      $("#navbar-toggler-navexpanded").css("margin-right", "20%");
+      console.log("navbar worked?");
+      console.log($("#navbar-toggler-navexpanded").css("margin"));
+    }
+
     $(".navbar").slideToggle();
   });
-  
+  // ^ ^ Controls the Navbar hiding and showing with the correct Css
+
+
+
+$(window).on("resize", function () {
+  const viewportHeight = $(window).height();
+  const navbar = $("#navbar").outerHeight(true);
+  const heroHeight = viewportHeight - navbar;
+  $("hero-image").height(heroHeight);
+});
+
+$(window).trigger("resize");
+
 
 
   var prevPage;
-  
+
   $(".navbar-button").click(function () {
-    var page = Math.floor(Math.random() * 6)
+    var page = Math.floor(Math.random() * 6);
     while (prevPage == page) {
       page = Math.floor(Math.random() * 6);
       console.log("it was the same this amount of times");
@@ -20,27 +44,27 @@ $(document).ready(function () {
       case 0:
         window.location.href = "index.html";
         console.log("index");
-       break;
+        break;
       case 1:
         window.location.href = "about.html";
         console.log("about");
-       break;
+        break;
       case 2:
         window.location.href = "education.html";
         console.log("education");
-       break;
+        break;
       case 3:
         window.location.href = "work.html";
         console.log("work");
-       break;
+        break;
       case 4:
         window.location.href = "interests.html";
         console.log("interests");
-       break;
+        break;
       case 5:
         window.location.href = "contact.html";
         console.log("contact");
-       break;
+        break;
     }
     prevPage = page;
   });
